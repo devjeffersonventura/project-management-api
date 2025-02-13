e4063f79534b85068201f40fa29cd11071b1499f
Jefferson <jefferson@educationjourney.com>
2025-02-13 01:36:36 -0300

    feat: add hasApiToken in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -30,19 +31,24 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
     /**
-     * Get the attributes that should be cast.
+     * Rules for validation.
      *
-     * @return array<string, string>
+     * @return array<string, mixed>
      */
-    protected function casts(): array
+    public function rules(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
         ];
     }
 }
