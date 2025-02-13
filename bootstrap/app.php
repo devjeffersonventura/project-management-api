702e593e999a98221f20dc040550d6379bdd0d2a
Jefferson <jefferson@educationjourney.com>
2025-02-13 01:35:24 -0300

    feat: custom cors config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use App\Http\Middleware\CustomCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'sanctum' => EnsureFrontendRequestsAreStateful::class,
+            'cors' => CustomCors::class,
         ]);  
     })
     ->withExceptions(function (Exceptions $exceptions) {
