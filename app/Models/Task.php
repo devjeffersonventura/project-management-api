a48fa96bb9e6336fe779b750fc69d1f627d2f117
Jefferson <jefferson@educationjourney.com>
2025-02-12 21:31:12 -0300

    feat: add rules to validate task data

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes; // Adicione esta linha
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class Task extends Model
 {
@@ -22,5 +24,39 @@ class Task extends Model
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+    public static function validate(array $data, $isUpdate = false)
+    {
+        $rules = [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'creation_date' => 'required|date',
+            'completion_date' => 'nullable|date|after_or_equal:creation_date',
+            'status' => 'required|in:pending,in_progress,completed',
+            'project_id' => 'required|exists:projects,id',
+        ];
+
+        if ($isUpdate) {
+            $rules = [
+                'title' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'creation_date' => 'sometimes|date',
+                'completion_date' => 'nullable|date|after_or_equal:creation_date',
+                'status' => 'sometimes|in:pending,in_progress,completed',
+                'project_id' => 'sometimes|exists:projects,id',
+            ];
+        }
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+    }
+
+    public function save(array $options = [])
+    {
+        $this->validate($this->attributes, $this->exists);
+        parent::save($options);
     }
 }
