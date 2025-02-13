38cf76ac42de8670a1c83a430e303466daba8b41
Jefferson <jefferson@educationjourney.com>
2025-02-13 02:45:07 -0300

    feat: add middleware sanctum to project routes

--- routes/project.php
+++ routes/project.php
@@ -3,4 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 
-Route::apiResource('projects', ProjectController::class);
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('projects', ProjectController::class)->except(['index', 'show']);
+});
