1116ed47c09049926f1d4e2103dcb74fd276ef87
Jefferson <jefferson@educationjourney.com>
2025-02-12 21:15:20 -0300

    feat: add rules to validate project data

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes; 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class Project extends Model
 {
@@ -18,8 +20,41 @@ class Project extends Model
         'status',
     ];
 
-    public function tasks()
+    public function project()
     {
-        return $this->hasMany(Task::class);
+        return $this->belongsTo(Project::class);
+    }
+
+    public static function validate(array $data, $isUpdate = false)
+    {
+        $rules = [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+            'status' => 'required|in:planned,in_progress,completed',
+        ];
+
+        if ($isUpdate) {
+            $rules = [
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_date' => 'sometimes|date',
+                'end_date' => 'sometimes|date|after_or_equal:start_date',
+                'status' => 'sometimes|in:planned,in_progress,completed',
+            ];
+        }
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+    }
+
+    public function save(array $options = [])
+    {
+        $this->validate($this->attributes, $this->exists);
+        parent::save($options);
     }
 }
