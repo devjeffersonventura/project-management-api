275c92daafe5e8aacd3daef65ecba04aa72693d8
Jefferson <jefferson@educationjourney.com>
2025-02-16 18:08:34 -0300

    feat: add base swagger configuration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,32 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Project Management API Documentation",
+ *     description="API documentation for Project Management System",
+ *     @OA\Contact(
+ *         email="your-email@example.com"
+ *     )
+ * )
+ * 
+ * @OA\Server(
+ *     url="http://localhost:8000/v1",
+ *     description="Local API Server"
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer"
+ * )
+ */
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
