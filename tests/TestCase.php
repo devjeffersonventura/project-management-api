7ab0cbac2d56d42b62ff453ac567fb54cf8a3faf
Jefferson <jefferson@educationjourney.com>
2025-02-17 18:24:14 -0300

    chore: setup test environment with DatabaseTransactions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,11 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+    use DatabaseTransactions; 
 }
